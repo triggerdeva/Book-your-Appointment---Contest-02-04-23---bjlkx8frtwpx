b4eaf84a23dede1a469876dc26bd21c4d4e58df8
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-02 13:28:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,49 +4,56 @@ import { addAppointment } from '../action';
 import '../styles/App.css';
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [time, setTime] = useState('');
-  const [description, setDescription] = useState('');
+const [name, setName] = useState('');
+const [time, setTime] = useState('');
+const [description, setDescription] = useState('');
+const dispatch = useDispatch();
+const appointments = useSelector(state => state.appointments);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+const handleSubmit = (e) => {
+e.preventDefault();
+const appointment = { name, time, description };
+dispatch(addAppointment(appointment));
+setName('');
+setTime('');
+setDescription('');
+};
 
-  return (
-    <div id="main">
-      <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input type="text" className="name-input"/>
-        <br />
-        <label>Time:</label>
-        <input type="text" className="time-input"/>
-        <br />
-        <label>Description:</label>
-        <textarea className="description-input"/>
-        <br />
-        <button type="submit">Add Appointment</button>
-      </form>
-      <br />
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Time</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-            {/* Appointment HTML */}
-            <tr>
-              <td>appointmentName</td>
-              <td>appointmentTime</td>
-              <td>appointmentDescription</td>
-            </tr>
-        </tbody>
-      </table>
-    </div>
-  )
+return (
+<div id="main">
+<form onSubmit={handleSubmit}>
+<label>Name:</label>
+<input type="text" className="name-input" value={name} onChange={(e) => setName(e.target.value)}/>
+<br />
+<label>Time:</label>
+<input type="text" className="time-input" value={time} onChange={(e) => setTime(e.target.value)}/>
+<br />
+<label>Description:</label>
+<textarea className="description-input" value={description} onChange={(e) => setDescription(e.target.value)}/>
+<br />
+<button type="submit">Add Appointment</button>
+</form>
+<br />
+<table>
+<thead>
+<tr>
+<th>Name</th>
+<th>Time</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+{appointments.map((appointment, index) => (
+<tr key={index}>
+<td>{appointment.name}</td>
+<td>{appointment.time}</td>
+<td>{appointment.description}</td>
+</tr>
+))}
+</tbody>
+</table>
+</div>
+)
 }
-
 
 export default App;
